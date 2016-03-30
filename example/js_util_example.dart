8ed80f97b0490853b6a0cb95672d99312f889dff
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-03-30 17:41:52 +0900

    Remove license from source files

--- example/js_util_example.dart
+++ example/js_util_example.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 library js_util.example;
 
 import 'package:js_util/js_util.dart';
--- lib/js_util.dart
+++ lib/js_util.dart
@@ -1,11 +1,3 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-/// The js_util library.
-///
-/// This is an awesome library. More dartdocs go here.
 library js_util;
-
-// TODO: Export any libraries intended for clients of this package.
 
 export 'src/js_util_base.dart';
